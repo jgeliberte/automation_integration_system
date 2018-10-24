5e9b487ddf6654107a856c65c4da32fb243460c8
John Geliberte <geliberteofficials@gmail.com>
2018-10-24 17:59:20 +0800

    test format

--- src/ais_main.py
+++ src/ais_main.py
@@ -2,15 +2,11 @@ from fbchat import Client
 from fbchat.models import *
 import configparser
 
-def initialize():
-	config = configparser.ConfigParser()
-	config.readfp(open('../config/credentials.cfg'))
-	client = Client(config['DYNASLOPE-AIS']['email'], config['DYNASLOPE-AIS']['password'])
-	return client
-
-def send_messenger():
-	client = initialize()
-	message_id = client.send(Message(text='Sample Message via Python-AIS'), thread_id='2199956213379612', thread_type=ThreadType.GROUP)
+def send_messenger(email, password, thread_id):
+	client = Client('%s', '%s') %(email, password)
+	message_id = client.send(Message(text='Sample Message via Python-AIS'), thread_id='%s', thread_type=ThreadType.GROUP) % thread_id
 
 if __name__ == "__main__":
-    send_messenger()
+	config = configparser.ConfigParser()
+	config.readfp(open('../config/credentials.cfg'))
+	send_messenger(config['DYNASLOPE-AIS']['email'], config['DYNASLOPE-AIS']['password'], config['DYNASLOPE-THREAD']['thread_id'])
