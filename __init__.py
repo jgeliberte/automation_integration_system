4e93afb69c0c86a5f035bcc6927ce51f86d1fbd3
John Geliberte <geliberteofficials@gmail.com>
2018-10-24 16:45:13 +0800

    Added config file and initpy

---
+++ __init__.py
@@ -0,0 +1,1 @@
+import ais_main
--- src/ais_main.py
+++ src/ais_main.py
@@ -1,6 +1,15 @@
 from fbchat import Client
 from fbchat.models import *
-client = Client('dynaslope.swat@gmail.com', 'asdfjkl;1234')
+from config import credentials as settings
 
-message_id = client.send(Message(text='Sample acknowledgement using reaction emoji sent via Python-AIS'), thread_id='2199956213379612', thread_type=ThreadType.GROUP)
-client.reactToMessage(message_id, MessageReaction.LOVE)
+
+def initialize():
+	client = Client('dynaslope.swat@gmail.com', 'asdfjkl;1234')
+
+def send_messenger():
+	initialize()
+	message_id = client.send(Message(text='Sample acknowledgement using reaction emoji sent via Python-AIS'), thread_id='2199956213379612', thread_type=ThreadType.GROUP)
+	client.reactToMessage(message_id, MessageReaction.LOVE)
+
+if __name__ == "__main__":
+    send_messenger()
