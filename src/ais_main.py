e06a8c15dd06b6e85ac39bd50d88e667d028cc53
John Geliberte <geliberteofficials@gmail.com>
2018-10-24 19:05:25 +0800

    Display msg id

--- src/ais_main.py
+++ src/ais_main.py
@@ -5,7 +5,8 @@ import configparser
 def send_messenger(email, password, thread_id):
 	client = Client(email, password)
 	message_id = client.fetchThreadList(offset=None, limit=20, thread_location=ThreadLocation.INBOX, before=None)
-	print(*message_id)
+	for x in message_id:
+		print x
 	# message_id = client.send(Message(text='Sample Message via Python-AIS'), thread_id=thread_id, thread_type=ThreadType.GROUP)
 
 if __name__ == "__main__":
